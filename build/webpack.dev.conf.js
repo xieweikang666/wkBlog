18b556c763d21d086ab88e846020d9052fc9544a
xieweikang666 <805936837@qq.com>
2019-10-06 20:03:22 +0800

    :art: 增加了项目icon

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -55,6 +55,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon: './static/favicon.ico',
       inject: true
     }),
     // copy custom static assets
@@ -85,8 +86,8 @@ module.exports = new Promise((resolve, reject) => {
           messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
         },
         onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
+          ? utils.createNotifierCallback()
+          : undefined
       }))
 
       resolve(devWebpackConfig)
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -67,7 +67,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         ? 'index.html'
         : config.build.index,
       template: 'index.html',
-
+      favicon: './static/favicon.ico',
       inject: true,
       minify: {
         removeComments: true,
